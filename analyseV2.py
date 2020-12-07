2cfc65f1c0485725f8c1c77a114c2f61f7e1b4ca
sruli <sruli@saurymper.com>
2020-12-07 16:47:16 +0000

    updated rewrote the ip in range part

--- analyseV2.py
+++ analyseV2.py
@@ -4,11 +4,12 @@ import ipaddress
 import datetime
 import operator
 
-#ip_source="/tmp/1.1"
+ip_source="/tmp/ip_test"
 #ip_ranges="/tmp/1.2"
-ip_source="sample_ip_list_sorted"
+#ip_source="sample_ip_list_sorted"
 ip_ranges="sample_range_list_sorted"
 report_file="/data/reports/ip_report_" # suffix is added from time
+
 
 def main():
 
@@ -23,79 +24,76 @@ def main():
     # Get the ip list into list
     with open(ip_source, "r") as ips:
         lines = ips.read().split('\n')
+#        lines = ips.readlines()
         for x in lines:
             if x:
                 ip.append(int(x))
 
     total_ip = len(ip)
 
-    first_m = 0
     with open(ip_ranges, "r") as ranges:
 #        range = ranges.readlines()
         range = ranges.read().split('\n')
         for r in range:
-            if r:
-                s = r.split(',')[0] # Range start
-                e = r.split(',')[1] # Range end
+            if ip:
+                if r:
+                    s = r.split(',')[0] # Range start
+                    e = r.split(',')[1] # Range end
 
-                # For efficiency - continue until first line (of range list) is in range of first ip in list
-                if first_m == 0:
-                    if int(e) < int(ip[0]):
+
+                    # For efficiency - continue until first line (of range list) is in range of first ip in list
+                    if int(e) < ip[0]:
                         continue
-                    else:
-                        first_m = 1
 
-                # For efficiency - break after line (of range list) is larger then last ip in list
-                if int(s) > int(ip[-1]):
-                    break
+                    # For efficiency - break after line (of range list) is larger then last ip in list
+                    if int(s) > ip[-1]:
+                        break
 
-                # Get the number of ip's matching the range
-                b = sum(int(s) < x < int(e) for x in ip) # is there a more efficient way?
-                if b > 0 :
+                    # Get the number of ip's matching the range by (by count of non matching)
+                    count_now = len(ip)
+                    count_less = 0
+                    count_more = 0
+                    ip_tmp = []
+                    for x in ip:
+                        if x < int(s):
+                            count_less += 1
+                            # add to fail list
+                            ip_fail.append(x)
+                        elif x > int(e):
+                            count_more += 1
+                            # add to temp list so next iteration the ip list is smaller
+                            ip_tmp.append(x)
+                    ip = ip_tmp
 
-                    c = r.split(',')[2] # iso code
-                    n = r.split(',')[3].strip() # full name
+                    b = count_now - (count_less + count_more)
+                    if b > 0 :
+                        c = r.split(',')[2] # iso code
+                        n = r.split(',')[3].strip() # full name
 
-                    # Add count to dict
-                    if not c in names:
-                        names[c] = n
-                        counts[c] = int(0)
-                    counts[c] += int(b)
-                    # remove these matched ip's from list (so at the end we have a list of non-matched ip's)
-                    ip_fail.append([x for x in ip if x <= int(s)]) # is there a more efficient way?
-
-                    # remove e ip's from main list to make it smaller
-                    ip[:] = [x for x in ip if x >= int(e)] # is there a more efficient way?
+                        # Add count to dict
+                        if not c in names:
+                            names[c] = n
+                            counts[c] = int(0)
+                        counts[c] += int(b)
 
     # Sort the failed list into single elements
     if ip_fail:
         for x in ip_fail:
-            if len(x) > 1:
-                for s in x:
-                    ip.append(s)
-            else:
-                ip.append(x)
+            ip.append(x)
 
     # sort the failed list
+    failed = []
     if ip:
         # Count the number of each failed ip
-        failed_tmp = []
         for x in ip:
-            c = ip.count(x)
-            failed_tmp.append(str(x) + ", " + str(c))
+            if not x in failed:
+                c = ip.count(x)
+                try:
+                    ipv4 = ipaddress.ip_address(int(x))
+                except:
+                    ipv4 = ("NOT ipv4 " + x)
+                failed.append(str(ipv4) + "," + str(c))
 
-        failed_tmp[:] = sorted(set(failed_tmp))
-
-        # convert the ip numbers back to ip's for report of failed matches
-        # failed = []
-        # for x in failed_tmp:
-        #     ipv4 = str(ipaddress.ip_address(x.split(',')[0]))
-        #     c = str(x.split(',')[1])
-        #     failed.append(str(x) + ", " + str(c))
-
-        failed = failed_tmp
-
-        # Sort the count highest first
     matched = dict( sorted(counts.items(), key=operator.itemgetter(1),reverse=True))
 
     now = datetime.datetime.now()
