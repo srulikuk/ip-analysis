6da8de542ff8fd44b73965170535547d6d8c82ab
sruli <sruli@saurymper.com>
2020-12-08 19:30:05 +0000

    config file

---
+++ config.py
@@ -0,0 +1,7 @@
+ip_source = "sample_ip_list" # path to ip list
+range_source_url = "https://www.ip2location.com/download/" # download url
+range_source_token = "yourTokenHere" # download token
+range_source_file = "DB1LITE" # download file code
+csv_file = "IP2LOCATION-LITE-DB1.CSV" # name of list file in downloaded zip file
+report_path = "/path/reports/ip_report_" # suffix is added from time
+delete_download = True # delete downloaded ip_range dir when done
